0a75fbf7c963e975cd182f4b5563598ab0456be4
msuc <98343588+msuccc@users.noreply.github.com>
2025-12-22 18:20:04 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+export default {
+  fetch(request) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/1189fb58638453b576ed5347ac1063a1.txt") {
+      return new Response(
+        "detectify",
+        {
+          headers: {
+            "content-type": "text/plain"
+          }
+        }
+      );
+    }
+
+    // normal site logic below
+    return new Response("OK");
+  }
+};
+
+
+/*
 import { router } from './router.js';
 import { corsHeaders } from './utils/cors.js';
 import { authenticate } from './middleware/auth.js';
@@ -30,4 +52,4 @@ export default {
       });
     }
   }
-};
+}; */
